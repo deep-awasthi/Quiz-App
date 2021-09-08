a5cb9a54e03fd8af71923d68beb529cd93e24c88
mad-skull <da.madskull@gmail.com>
2021-09-08 17:22:57 +0530

    constant data class

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -2,6 +2,7 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +10,7 @@ class QuizQuestionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz_questions)
 
         val questionsList = Constants.getQuestions()
+
+        Log.i("Questions Size","${questionsList.size}")
     }
 }
